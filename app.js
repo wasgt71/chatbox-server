e43c4614d5f13ac356aeee182c7142f4765821ef
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-12-24 13:07:33 -0500

    edited cors.

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://nuuserochatbot.netlify.app/", "http://localhost:5173"],
-    methods: "GET,POST",
-    allowedHeaders: "Content-Type",
+    origin: "https://nuuserochatbot.netlify.app",
+    methods: ["GET,POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
