819562092739ee9cb176b7be992c5f5061269e83
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-12-24 02:33:07 -0500

    changed cors origin to client side link.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://aicb-client.onrender.com",
     methods: "GET,POST",
     allowedHeaders: "Content-Type",
   })
